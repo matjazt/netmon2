8b9d67cd2c4ae84996dfba8f4673f754758b9571
matjazt <matjaz.terpin@gmail.com>
2026-01-16 14:05:35 +0100

    feat: add swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	runtimeOnly("org.postgresql:postgresql") 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
